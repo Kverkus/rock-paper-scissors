8bc65ce29549dc66b831145d4bcd31e979fa8a7b
Kverkus <kverk54@gmail.com>
2024-12-12 06:18:50 +0700

    Add input for player turns

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ for (let i = 0; i < 10; i++) {
   console.log(getComputerChoice());
 }
 
+console.log(getHumanChoice());
+
+function getHumanChoice () {
+  let choice;
+  let isChoiceDone = false;
+
+  while (isChoiceDone !== true) {
+    choice = prompt("Choose rock, paper or scissors.", "rock");
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+      alert("Incorrect choice. You must choose only rock, paper or scissors.");
+    }
+    else {
+      isChoiceDone = true;
+    }
+  }
+  return choice;
+}
+
 function getComputerChoice () {
   let roll = getRandomInt(3);
   let choice;
