57a149082cea7cab33a4b674a569433ef71c1453
Kverkus <kverk54@gmail.com>
2024-12-12 05:49:04 +0700

    Add a logic for random turns from bot

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi');
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice());
+}
+
+function getComputerChoice () {
+  let roll = getRandomInt(3);
+  let choice;
+
+  if (roll === 0) {
+    choice = "rock";
+  }
+  else if (roll === 1) {
+    choice = "paper";
+  }
+  else {
+    choice = "scissors";
+  }
+
+  return choice;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
